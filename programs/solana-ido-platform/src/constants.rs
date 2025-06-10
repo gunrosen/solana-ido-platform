0a8f2090fe36a28646d0ee2147ed0edfe71fc8ec
hulk19 <phamhungbkhn@gmail.com>
2025-06-10 18:09:56 +0700

    update: first Initialize struct

---
+++ programs/solana-ido-platform/src/constants.rs
@@ -0,0 +1,4 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+pub const CONFIG_CREATOR: &[u8] = b"CONFIG_CREATOR";
--- programs/solana-ido-platform/src/lib.rs
+++ programs/solana-ido-platform/src/lib.rs
@@ -1,15 +1,23 @@
 use anchor_lang::prelude::*;
+pub mod constants;
 
+pub use constants::*;
 declare_id!("9ghHF9QjUqzR9RLMasz6niBTQyCD6TMpc85yy1g2ioo6");
 
 #[program]
 pub mod solana_ido_platform {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    /// CHECK:
+    pub creator: UncheckedAccount<'info>,
+}
