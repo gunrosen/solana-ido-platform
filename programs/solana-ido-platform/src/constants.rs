05741a0422bedb57585a6157dbf54077c9505e6e
hulk19 <phamhungbkhn@gmail.com>
2025-06-11 18:32:17 +0700

    update: add creator, owner

--- programs/solana-ido-platform/src/constants.rs
+++ programs/solana-ido-platform/src/constants.rs
@@ -2,3 +2,6 @@ use anchor_lang::prelude::*;
 
 #[constant]
 pub const CONFIG_CREATOR: &[u8] = b"CONFIG_CREATOR";
+
+#[constant]
+pub const CONFIG_SEED: &[u8] = b"ido_platform_seed";
--- programs/solana-ido-platform/src/lib.rs
+++ programs/solana-ido-platform/src/lib.rs
@@ -1,23 +1,19 @@
 use anchor_lang::prelude::*;
+pub mod instructions;
+pub mod states;
 pub mod constants;
 
+pub use instructions::{ initialize::* };
+pub use states::*;
 pub use constants::*;
+
 declare_id!("9ghHF9QjUqzR9RLMasz6niBTQyCD6TMpc85yy1g2ioo6");
 
 #[program]
 pub mod solana_ido_platform {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn initialize(_ctx: Context<Initialize>, owner: Pubkey, creator: Pubkey) -> Result<()> {
+         process_initialize(ctx, owner, creator)
     }
-}
-
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(mut)]
-    pub owner: Signer<'info>,
-
-    /// CHECK:
-    pub creator: UncheckedAccount<'info>,
 }
